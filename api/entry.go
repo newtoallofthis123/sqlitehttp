8edc5bab32f2d8d56e8eaaf7d827f5750ad304be
newtoallofthis123 <noobscience123@gmail.com>
2024-11-03 13:42:28 +0530

    Send and Tables work

--- api/entry.go
+++ api/entry.go
@@ -1,13 +1,28 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/newtoallofthis123/sqlite_http/db"
+)
 
 type Api struct {
 	ListenAddr string
+	Db         *db.Db
 }
 
 func NewApi(listenAddr string) *Api {
-	return &Api{ListenAddr: listenAddr}
+	d, err := db.NewDb("test.db")
+	if err != nil {
+		panic(err)
+	}
+	err = d.Discover()
+	if err != nil {
+		panic(err)
+	}
+
+	return &Api{ListenAddr: listenAddr, Db: d}
 }
 
 func (a *Api) ping(c *gin.Context) {
@@ -16,10 +31,59 @@ func (a *Api) ping(c *gin.Context) {
 	})
 }
 
+func (a *Api) handleCommandSend(c *gin.Context) {
+	cmd := c.PostForm("cmd")
+
+	if cmd == "" {
+		c.JSON(400, gin.H{
+			"error": "no command provided",
+		})
+		return
+	}
+
+	if strings.HasPrefix(cmd, "SELECT") {
+		rows, err := a.Db.RunQuery(cmd)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"rows": rows,
+		})
+		return
+	} else {
+		r, err := a.Db.RunExec(cmd)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		rowsEffected, _ := r.RowsAffected()
+
+		c.JSON(200, gin.H{
+			"message": rowsEffected,
+		})
+		return
+	}
+}
+
+func (a *Api) handleTables(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"tables": a.Db.RowsInfo,
+	})
+}
+
 func (a *Api) Run() {
 	r := gin.Default()
 
 	r.GET("/ping", a.ping)
+	r.POST("/send", a.handleCommandSend)
+	r.GET("/tables", a.handleTables)
 
 	r.Run(a.ListenAddr)
 }
